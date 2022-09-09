d98f717c07a4a2f8493ac1fdc755c835d9e564a7
mbh <itstudent.mbh@gmail.com>
2022-09-09 08:36:05 +0430

    create migrations

--- database/migrations/2022_09_09_035710_create_images_table.php
+++ database/migrations/2022_09_09_035710_create_images_table.php
@@ -13,12 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname');
-            $table->string('lastname');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->foreignId('user_id')->constrained();
+            $table->string('path')->unique();
+            $table->string('title')->unique();
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('images');
     }
 };
